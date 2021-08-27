df44bc5a032fa8a37655f2e17b3d0624dd164eea
Dave Hunter <thebeakking@gmail.com>
2021-08-27 12:27:32 +0100

    restructure folders to use versions

--- app/v1.js
+++ app/v1.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-const v1 = require('./views/register-application/v1/_routes');
-router.use('/register-application/v1', v1);
+router.get('/hello-world',(req, res)=>res.send('hello'))
 
 // Add your routes above the module.exports line
 module.exports = router
