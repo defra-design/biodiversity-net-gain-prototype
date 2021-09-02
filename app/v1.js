b623247a96462d438fb664b5cbcda64cbe863dc6
Dave Hunter <thebeakking@gmail.com>
2021-09-02 12:55:29 +0100

    remove routes v1 file

--- app/v1.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-router.get('/hello-world',(req, res)=>res.send('hello'))
-
-// Add your routes above the module.exports line
-module.exports = router
