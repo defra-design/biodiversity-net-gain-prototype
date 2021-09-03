3be9b77c72c1ab2aae38ecf76f399a5203e25134
Dave Hunter <thebeakking@gmail.com>
2021-09-03 16:20:48 +0100

    add routes for search

--- app/routes.js
+++ app/routes.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
-const v1 = require('./views/register-application/v1/_routes');
-router.use('/register-application/v1', v1);
+const registerapplicationv1 = require('./views/register-application/v1/_routes');
+router.use('/register-application/v1', registerapplicationv1);
+
+const registerv1 = require('./views/register/v1/_routes');
+router.use('/register/v1', registerv1);
 
 // Add your routes above the module.exports line
 module.exports = router
