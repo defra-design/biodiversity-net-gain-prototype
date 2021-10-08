671d78d1f09245a19864f809131955dc6775ffa8
Dave Hunter <thebeakking@gmail.com>
2021-10-08 15:08:41 +0100

    update map poc with vector data

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,10 +1,21 @@
+import {Style, Fill, Stroke} from 'ol/style';
+
 import GeoJSON from 'ol/format/GeoJSON';
 import Map from 'ol/Map';
 import VectorLayer from 'ol/layer/Vector';
 import VectorSource from 'ol/source/Vector';
 import View from 'ol/View';
 
-new Map({
+// Drag and drop functionality
+import DragAndDrop from 'ol/interaction/DragAndDrop'
+// Drawing new features
+import Draw from 'ol/interaction/Draw';
+import Snap from 'ol/interaction/Snap';
+
+
+const source = new VectorSource();
+
+const map = new Map({
   target: 'map',
   layers: [
     new VectorLayer({
@@ -18,4 +29,43 @@ new Map({
     center: [0, 0],
     zoom: 2,
   }),
+});
+
+const layer = new VectorLayer({
+  source: source,
+});
+map.addLayer(layer);
+
+map.addInteraction(
+  new DragAndDrop({
+    source: source,
+    formatConstructors: [GeoJSON],
+  })
+);
+
+map.addInteraction(
+  new Draw({
+    type: 'Polygon',
+    source: source,
+  })
+);
+
+map.addInteraction(
+  new Snap({
+    source: source,
+  })
+);
+
+const clear = document.getElementById('clear');
+clear.addEventListener('click', function () {
+  source.clear();
+});
+
+const format = new GeoJSON({featureProjection: 'EPSG:3857'});
+const download = document.getElementById('download');
+source.on('change', function () {
+  const features = source.getFeatures();
+  const json = format.writeFeatures(features);
+  download.href =
+    'data:application/json;charset=utf-8,' + encodeURIComponent(json);
 });
