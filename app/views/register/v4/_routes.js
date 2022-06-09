751e5454f13a2a6b6528535940a498fff911813a
Dave Hunter <thebeakking@gmail.com>
2022-06-09 13:51:03 +0100

    update public register v4 search logic

--- app/views/register/v4/_routes.js
+++ app/views/register/v4/_routes.js
@@ -6,24 +6,15 @@ router.post('/search-answer', function (req, res) {
 
   var install = req.session.data['search-register']
 
-  if (install == "150384"){
-    res.redirect('/register/v4/site-a')
-  } else {
+  if (install == "BNG-220115001"){
+    res.redirect('/register/v4/offsite-a')
+  } else if (install == "21/02367/FUL"){
+    res.redirect('/register/v4/development-a')
+  }  else {
     res.redirect('/register/v4/search-results')
   }
 })
 
-// Search planning ref
-router.post('/search-answer-planning-ref', function (req, res) {
-
-  var install = req.session.data['search-register']
-
-  if (install == "150384"){
-    res.redirect('/register/v4/development-a')
-  } else {
-    res.redirect('/register/v4/search-results-planning-ref')
-  }
-})
 
 // Add your routes above the module.exports line
 module.exports = router
