7127bc378555ae74e217b24296d590f88157dbf6
Dave Hunter <thebeakking@gmail.com>
2022-06-24 13:21:20 +0100

    remove routing logiv public register v5

--- app/views/register/v5/_routes.js
+++ app/views/register/v5/_routes.js
@@ -1,20 +1,5 @@
 const express = require('express')
 const router = express.Router()
 
-// Search
-router.post('/search-answer', function (req, res) {
-
-  var install = req.session.data['search-register']
-
-  if (install == "BNG-220115001"){
-    res.redirect('/register/v5/offsite-a')
-  } else if (install == "21/02367/FUL"){
-    res.redirect('/register/v5/development-a')
-  }  else {
-    res.redirect('/register/v5/search-results')
-  }
-})
-
-
 // Add your routes above the module.exports line
 module.exports = router
