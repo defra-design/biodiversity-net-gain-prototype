b7f019e97a50a0be16b255e5ced5286fb989a36b
Dave Hunter <thebeakking@gmail.com>
2022-09-08 14:45:11 +0100

    update passing data on legal ageement and hmmp

--- app/filters.js
+++ app/filters.js
@@ -41,5 +41,94 @@ module.exports = function (env) {
   /* ------------------------------------------------------------------
     keep the following line to return your filters to the app
   ------------------------------------------------------------------ */
+
+  filters.toArray = val => typeof val === 'string' ? [val] : val;
+
+  // Filter to change a number, like 1 "12" or 04, to a month in words
+  //
+  // Accepts numbers, strings, data[] and variables
+  // If empty prints nothing unless you give it a placeholder which it will print out
+  //
+  // Usage in your templates:
+  //
+  // With number with filer
+  //                    {{ 1 | monthToWord }}
+  // With data from previous input
+  //                    {{ data['dob-month'] | monthToWord }}
+  // With data that's empty with alternative placeholder
+  //                    {{ data['dob-month'] | monthToWord('January') }}
+
+  filters.monthToWord = function(month,placeholder) {
+    var months = {
+      "1":'January',
+      "01":'January',
+      "2":'February',
+      "02":'February',
+      "3":'March ',
+      "03":'March',
+      "4":'April',
+      "04":'April',
+      "5":'May',
+      "05":'May',
+      "6":'June',
+      "06":'June',
+      "7":'July',
+      "07":'July',
+      "8":'August',
+      "08":'August',
+      "9":'September',
+      "09":'September',
+      "10":'October',
+      "11":'November',
+      "12":'December'
+    }
+
+    if (month) {
+      month.toString()
+      return months[month]
+    } else if (placeholder) {
+      return placeholder
+    } else {
+      return
+    }
+
+  }
+
+  filters.monthToWordWithSpaces = function(month,placeholder) {
+    var months = {
+      "1":'January',
+      "01":'January',
+      "2":'February',
+      "02":'February',
+      "3":'March ',
+      "03":'March',
+      "4":'April',
+      "04":'April',
+      "5":'May',
+      "05":'May',
+      "6":'June',
+      "06":'June',
+      "7":'July',
+      "07":'July',
+      "8":'August',
+      "08":'August',
+      "9":'September',
+      "09":'September',
+      "10":'October',
+      "11":'November',
+      "12":'December'
+    }
+
+    if (month) {
+      month.toString()
+      return ' ' + months[month] + ' '
+    } else if (placeholder) {
+      return ' ' + placeholder + ' '
+    } else {
+      return
+    }
+
+  }
+
   return filters
 }
